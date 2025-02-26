7539668f2a5394898a175a4e949a36a54b45cebe
Cao Anh <nguyenmanhcaoanh9103@gmail.com>
2025-02-26 16:27:06 +0700

    fix every thing

--- src/main/java/com/kma/Application.java
+++ src/main/java/com/kma/Application.java
@@ -1,5 +1,6 @@
 package com.kma;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import java.util.TimeZone;
 
 @SpringBootApplication
 public class Application {
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
     public static void main(String[] args) {
+
         // Set timezone mặc định là UTC
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(Application.class, args);
